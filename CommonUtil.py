1b5adb292fe876b2dd20c8122de62482dc9b7d4f
acidpop <acidpop@acidpop.kr>
2016-09-19 03:40:07 +0900

    Version Check modify

--- CommonUtil.py
+++ CommonUtil.py
@@ -8,12 +8,19 @@ def hbytes(num):
         num /= 1024.0
     return "%3.2f%s" % (num, 'TB')
 
+
+def dequote(s):
+    if (s[0] == s[-1]) and s.startswith(("'", '"')):
+        return s[1:-1]
+    return s
+
+
 def GetDSMMajorVersion():
     parseVars = {}
     with open("/etc/VERSION") as versionFile:
          for line in versionFile:
              key, value = line.partition("=")[::2]
-             parseVars[key.strip()] = value.strip()
+             parseVars[key.strip()] = dequote(value.strip())
 
     return parseVars['majorversion']
 
